9f4d680fb66e0a45946b1977f7de793b9305de29
Philip <philippernf@gmail.com>
2018-04-05 13:56:00 +0100

    styling icon-rows

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 $(() => {
 
   $('#fullpage').fullpage({
-    menu: '#myMenu'
+    // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    anchors: ['home', 'projects', 'skills', 'about'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+    // paddingTop: '3em'
+    // fixedElements: '#menu'
   });
 
 });
